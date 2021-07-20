0aad5981a3a54f858a0fb6a9033da2d0ee75c0ca
Blue <bkioshn@gmail.com>
2021-07-20 17:02:46 +0700

    Fix lisener

--- evm-listener.py
+++ evm-listener.py
@@ -2,12 +2,34 @@ import os
 from web3 import Web3, HTTPProvider
 import psycopg2
 import json
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import func, inspect
+import time
 
-w3 = Web3(HTTPProvider(
-    "https://kovan.infura.io/v3/" + os.getenv("PROJECTID")))
+# Starting block of the smart contract
+FROM_BLOCK = 25040009
+
+class Config(object):
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://bkioshn:@localhost:5432/postgres"
+
+app = Flask(__name__)
+app.config.from_object(Config())
+db = SQLAlchemy(app)
 
 
-if w3.isConnected():
+class Vrf(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    caller = db.Column(db.String, nullable=False)
+    seed = db.Column(db.String, nullable=False)
+    task_key = db.Column(db.String, nullable=False)
+    bounty = db.Column(db.String, nullable=False)
+    time = db.Column(db.Integer, nullable=False)
+
+
+def request_data(w3, cursor):
+    global FROM_BLOCK
     # VRF provider contract
     provider_contract_id = "0xD1785fd50c2DBF77bF5F376ECc960BB0E9c19f14"
 
@@ -17,33 +39,55 @@ if w3.isConnected():
     provider_contract = w3.eth.contract(
         address=provider_contract_id, abi=provider_abi)
 
-    # FIXME Should listen to the latest block
-    # Now is just trying to get some data
-    myfilter = provider_contract.events.RandomDataRequested.createFilter(
-        fromBlock=25040009, toBlock=25040009)
+    while (True):
+        print("timer start")
+        print(FROM_BLOCK)
+        time.sleep(60)
+        myfilter = provider_contract.events.RandomDataRequested.createFilter(
+            fromBlock=FROM_BLOCK, toBlock="latest")
 
-    event_list = myfilter.get_all_entries()
-    request_info = event_list[0].args
+        event_list = myfilter.get_all_entries()
+        if (len(event_list) > 0):
+            print("check len'")
+            insert_to_db(event_list, cursor)
 
-    # Sending to database
-    try:
-        connection = psycopg2.connect(user="postgres",
-                                      host="127.0.0.1",
-                                      port="5432",
-                                      database="postgres")
+def insert_to_db(event_list, cursor):
+    global FROM_BLOCK
 
-        cursor = connection.cursor()
+    if not (db.engine.has_table("vrf")):
+        db.create_all()
+    for event in event_list:
+        FROM_BLOCK = event.blockNumber
 
+        print('block number ', event.blockNumber)
+        request_info = event.args
         # Insert data
         try:
-            cursor.execute("""INSERT INTO vrf (Caller, Seed, TaskKey, Bounty, Time) VALUES (%s, %s, %s, %s, %s);""", (
-                request_info.caller, request_info.seed, request_info.taskKey, request_info.bounty, request_info.time))
+            request = Vrf(caller=request_info.caller, seed=request_info.seed, task_key=request_info.taskKey, bounty=request_info.bounty, time=request_info.time)
+            db.session.add(request)
+            db.session.commit()
         except:
             print("Cant insert")
 
-        # Test whether data is added or not
-        cursor.execute("""SELECT * FROM vrf where time = 12345678""")
-        rows = cursor.fetchall()
-        print(rows)
-    except:
-        print("Not response")
+        # cursor.execute("""SELECT * FROM vrf where time = 12345678""")
+        # rows = cursor.fetchall()
+        # print(rows)
+    
+    
+
+def main():
+    w3 = Web3(HTTPProvider(
+        "https://kovan.infura.io/v3/" + os.getenv("PROJECTID")))
+
+    connection = psycopg2.connect(user="postgres",
+                                  host="127.0.0.1",
+                                  port="5432",
+                                  database="postgres")
+    if w3.isConnected() and connection.status:
+        cursor = connection.cursor()
+        request_data(w3, cursor)
+
+
+if __name__ == '__main__':
+    main()
+
